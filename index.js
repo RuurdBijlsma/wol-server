ec7779c4464b185c83c4227e75f58617a7d6f7fd
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-03-08 18:01:06 +0100

    Update mac adress format

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const wol = require('node-wol');
 
-wol.wake('B4-2E-99-31-BE-4E', function (error) {
-    if (error) {
-        console.log("packet not sent", error);
-    } else {
-        console.log("packet sent");
+wol.wake('B4:2E:99:31:BE:4E', {
+    address: '192.168.178.18',
+    port: 7
+}, function(error) {
+    if(error) {
+        console.log("Success", error);
+        return;
+    }else{
+        console.log("Success");
     }
 });
+console.log("Done!");
