df8664c7408075e6240096374781403dce1953f1
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-03-08 18:08:13 +0100

    update remove ip

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const wol = require('node-wol');
 
 wol.wake('B4:2E:99:31:BE:4E', {
-    address: '192.168.178.18',
-    port: 7
+    // address: '192.168.178.18',
+    // port: 7
 }, function (error) {
     if (error) {
         console.log("Error", error);
